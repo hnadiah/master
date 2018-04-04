8aab5e475d7c92e08628b07a8121da9e6d3d9f21
hnadiah <38072782+hnadiah@users.noreply.github.com>
2018-04-04 20:42:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-test
+<?php
+include_once("header.php");
+include_once("sidebar.php");
+?>
+
+    <!-- Page Content -->
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Utama</h1>
+                    <!-- start data here -->
+
+
+                    <div class="row">
+                        <div class="well text-center">
+                            <div class=" text-left">
+                                
+                            </div>
+
+                        </div>
+                        <!-- /.well -->
+                    </div>
+
+                    <!-- end data here -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
+
+    <?php
+include_once("footer.php");
+?>
+
